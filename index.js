f523e6f42a91659dff4b97c8a02866a38fd94b9b
Anthea <el1811756@gmail.com>
2020-10-14 12:36:11 +1100

    index.html

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const PORT = process.env.PORT;
 var http = require('http');
+let fs = require('fs');
 
 http.createServer(function (req, res) {
-    response.writeHead(200, {
+    res.writeHead(200, {
         'Content-Type': 'text/html'
     });
-    fs.readFile('index.html', null, function (error, data) {
+    fs.readFile('./index.html', null, function (error, data) {
         if (error) {
-            response.writeHead(404);
-            respone.write('Whoops! File not found!');
+            res.writeHead(404);
+            res.write('Whoops! File not found!');
         } else {
-            response.write(data);
+            res.write(data);
         }
-        response.end();
+        res.end();
     });
 }).listen(PORT);
