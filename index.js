6f26a3b48b072ca32dac9f3c5f5b7cef7ccb00ba
Anthea <el1811756@gmail.com>
2020-10-14 12:16:47 +1100

    add index.html

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const PORT = process.env.PORT;
 var http = require('http');
 
 http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World!');
+    response.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+    fs.readFile('index.html', null, function (error, data) {
+        if (error) {
+            response.writeHead(404);
+            respone.write('Whoops! File not found!');
+        } else {
+            response.write(data);
+        }
+        response.end();
+    });
 }).listen(PORT);
