87ed000141c78ed60e2747de007e33d0b8fb334a
Anthea <el1811756@gmail.com>
2020-10-14 14:05:47 +1100

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ http.createServer(function (req, res) {
     res.writeHead(200, {
         'Content-Type': 'text/html'
     });
-    fs.readFile('/web/index.html', null, function (error, data) {
+    fs.readFile('./web/index.html', null, function (error, data) {
         if (error) {
             res.writeHead(404);
             res.write('Whoops! File not found!');
