6e64162fc34cbaa13ea3a5bb9e1348c4ec66c373
Anthea <el1811756@gmail.com>
2020-10-15 21:15:50 +1100

    signin signup tab added

--- index.js
+++ index.js
@@ -12,6 +12,12 @@ http.createServer(function (req, res) {
     } else if (req.url === '/doctors.html') {
         res.writeHead(200, { 'Content-Type': 'text/html' });
         fs.createReadStream(__dirname + '/web/doctors.html').pipe(res);
+    } else if (req.url === '/signin.html') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        fs.createReadStream(__dirname + '/web/signin.html').pipe(res);
+    } else if (req.url === '/signup.html') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        fs.createReadStream(__dirname + '/web/signup.html').pipe(res);
     } else {
         res.writeHead(404, { 'Content-Type': 'text/html' });
         fs.createReadStream(__dirname + '/web/404.html').pipe(res);
