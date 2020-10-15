3060a09b3c72120e910c294fdd2d16f27f5cf2d2
Anthea <el1811756@gmail.com>
2020-10-15 20:59:43 +1100

    Update index.js

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ let fs = require('fs');
 http.createServer(function (req, res) {
     if (req.url === 'https://rehabilitation-iot2020.herokuapp.com//index.html' || req.url === 'https://rehabilitation-iot2020.herokuapp.com//') {
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        fs.createReadStream(__dirname + './web/index.html').pipe(res);
+        fs.createReadStream(__dirname + '/web/index.html').pipe(res);
     } else if (req.url === '/programs.html') {
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        fs.createReadStream(__dirname + './web/programs.html').pipe(res);
+        fs.createReadStream(__dirname + '/web/programs.html').pipe(res);
     } else if (req.url === '/doctors.html') {
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        fs.createReadStream(__dirname + './web/doctors.html').pipe(res);
+        fs.createReadStream(__dirname + '/web/doctors.html').pipe(res);
     } else {
         res.writeHead(404, { 'Content-Type': 'text/html' });
         fs.createReadStream(__dirname + '/404.html').pipe(res);
