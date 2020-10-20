cf92a35e08cc62e51c821043bc537e0264cfaeae
MikeyZee <michaelzenkis@hotmail.com>
2020-10-20 22:18:43 +1100

    Remove old routing

--- index.js
+++ index.js
@@ -46,31 +46,3 @@ var server = app.listen(port, function() {
 
     console.log("App listening at http://%s:%s", host, port)
 })
-
-/*
-http.createServer(function (req, res) {
-    if (req.url === '/index.html' || req.url === '/') {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        fs.createReadStream(__dirname + '/web/index.html').pipe(res);
-    } else if (req.url === '/programs.html') {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        fs.createReadStream(__dirname + '/web/programs.html').pipe(res);
-    } else if (req.url === '/doctors.html') {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        fs.createReadStream(__dirname + '/web/doctors.html').pipe(res);
-    } else if (req.url === '/signin.html') {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        fs.createReadStream(__dirname + '/web/signin.html').pipe(res);
-    } else if (req.url === '/signup.html') {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        fs.createReadStream(__dirname + '/web/signup.html').pipe(res);
-    } else if (req.url === '/patientDashboard.html') {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        fs.createReadStream(__dirname + '/web/patientDashboard.html').pipe(res);
-    } else {
-        res.writeHead(404, { 'Content-Type': 'text/html' });
-        fs.createReadStream(__dirname + '/web/404.html').pipe(res);
-    }
-}).listen(PORT);
-
-*/
